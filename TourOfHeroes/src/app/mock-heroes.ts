c7683d349f8f1ca523c62a6054ab6211324f3c68
Chris Elliott <chris@fuzzylojic.com>
2021-11-06 14:33:52 -0700

    display a list

---
+++ TourOfHeroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Thor' },
+  { id: 12, name: 'Captain America' },
+  { id: 13, name: 'Scarlet Witch' },
+  { id: 14, name: 'Captain Marvel'},
+  { id: 15, name: 'Hawkeye'},
+  { id: 16, name: 'Black Widow'},
+  { id: 17, name: 'Black Panther'},
+  { id: 18, name: 'Ironman'},
+  { id: 19, name: 'Shang Chi'},
+  { id: 20, name: 'Hulk'}
+]
